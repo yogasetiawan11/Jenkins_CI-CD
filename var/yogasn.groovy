1c734c28964a93d52d47c09a996ad7dfebf957bc
yoga.s <setiawan4business@gmail.com>
2025-07-27 20:30:41 +0700

    update jenkins ci cd

---
+++ var/yogasn.groovy
@@ -0,0 +1,5 @@
+// enter syntax definition for a Jenkins pipeline step
+def call() {
+    sh 'echo Hello My naeme is yoga'
+}
+// You have this stored this file with groovy extension in the `vars` directory of your Jenkins shared library.
